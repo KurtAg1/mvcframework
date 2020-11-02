f2977e35e2baf234b842e889c0fa3e01fe461c53
KurtAg1 <kurtagius613@gamil.com>
2020-11-02 18:46:56 +0100

    adding files

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// Database params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'mvcframework');
+
+// APP ROOT
+define('APPROOT', dirname(__FILE__, 2));
+
+// URL ROOT (Dynamic links)
+define('URLROOT', 'localhost/mvcframework');
+
+// Sitename
+define('SITENAME', 'MVC Framework');
